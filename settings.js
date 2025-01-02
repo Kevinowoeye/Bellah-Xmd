accac37f6444ca635eda855f7c9fb9e4fb2133dd
Kevinowoeye <165365587+Kevinowoeye@users.noreply.github.com>
2025-01-02 15:24:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || '' 
 //owmner v card
 global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
 global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
@@ -16,7 +16,7 @@ global.location = "Kenya" //ur location
 
 //new
 global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
+global.ownernumber = process.env.OWNER_NUMBER ||'2349125096374' //ur owner number
 global.ownername = '© TENNOMODZ' //ur owner name
 global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
 global.wagc = "https://chat.whatsapp.com/Hs0AwkOaFzbGi5sjicdeTR" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
@@ -27,13 +27,13 @@ global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers aut
 global.author = "Giddy-Tennor"
 global.creator = "254703726139@s.whatsapp.net"
 global.xprefix = process.env.PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.premium = ["2349125096374"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = process.env.TYPE_MENU || 'v2' // menu type 'v1' => 'v8' 
 global.typereply = process.env.TYPE_REPLY ||'v1' // reply type 'v1' => 'v3'
-global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
+global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '' //set autoblock country code
 global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
 global.welcome = process.env.WELCOME || 'false' //welcome/left in groups
 global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
@@ -41,7 +41,7 @@ global.autoswview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
 global.adminevent = true //show promote/demote message
 global.groupevent = true //show update messages in group chat
 //msg
-global.autorecord = process.env.AUTO_RECORD || 'false'
+global.autorecord = process.env.AUTO_RECORD || 'true'
 global.autorecordtype = process.env.AUTO_RECORDTYPE || 'false'
 
 global.mess = {
